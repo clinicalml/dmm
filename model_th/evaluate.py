4c234ae65576062b130b64f4fb27d823adaadb58
Rahul G. Krishnan <rahulk90@gmail.com>
2017-08-07 22:10:42 -0400

    updated eval + learning script

--- model_th/evaluate.py
+++ model_th/evaluate.py
@@ -5,13 +5,11 @@ import time
 from common_eval_utils import *
 
 """
-Functions for evaluating a DMM object
+Functions for evaluating DMMs
 """
-
 def infer(dmm, dataset):
     """ Posterior Inference using recognition network 
     Returns: z,mu,logcov (each a 3D tensor) 
-    Remember to multiply each by the mask of the dataset (if applicable)! 
     """
     dmm.resetDataset(dataset, quiet=True)
     return dmm.posterior_inference(idx=np.arange(dataset['features']['tensor'].shape[0]))
@@ -40,38 +38,31 @@ def evaluateBound(dmm, dataset, batch_size=100):
     dmm._p(('(Evaluate) Validation Bound: %.4f [Took %.4f seconds]')%(bound,end_time-start_time))
     return bound
 
-def sample(dmm, B, U, ftypes, nsamples=100, T=20, additional = {}, stochastic = True):
-    """
-    Sample from Generative Model
-    * We need to have baseline information about patient
-    """
+def sample(dmm, nsamples=100, T=20, additional = {}, stochastic = True):
+    """ Sample from Generative Model """
     assert T>1, 'Sample atleast 2 timesteps'
-    #Initial sample
-    mu0, cov0 = dmm.init_prior(B[:nsamples].astype('float32'))
-    actions = None
-    if dmm.params['dim_actions']>0:
-        actions = setupU(U[:nsamples])
+    mu0       = np.zeros((nsamples, dmm.params['dim_stochastic']))
+    cov0      = np.ones_like(mu0)
     if stochastic:
         z       = sampleGaussian(mu0, np.log(cov0))
     else:
         z       = mu0
-    all_zs = [np.copy(z)]
-    mulist  = [mu0]
-    covlist = [cov0]
+    all_zs  = [np.copy(z)[:,None,:]]
+    mulist  = [mu0[:,None,:]]
+    covlist = [cov0[:,None,:]]
     for t in range(T-1):
-        if dmm.params['dim_actions']==0:
-            mu, cov  = dmm.transition_fxn(Z = z)
-        else:
-            mu, cov  = dmm.transition_fxn(Z=z, U=actions[:,[t],:])
+        mu, cov  = dmm.transition_fxn(Z = z)
         if stochastic:
             z        = sampleGaussian(mu,np.log(cov))
         else:
             z        = mu 
-        all_zs.append(np.copy(z))
-        mulist.append(np.copy(mu))
-        covlist.append(np.copy(cov))
-    zvec                 = np.concatenate(all_zs,axis=1)
+        all_zs.append(np.copy(z)[:,None,:])
+        mulist.append(np.copy(mu)[:,None,:])
+        covlist.append(np.copy(cov)[:,None,:])
+    zvec                        = np.concatenate(all_zs,axis=1)
     additional['mu_sample']     = np.concatenate(mulist, axis=1)
     additional['cov_sample']    = np.concatenate(covlist, axis=1)
-    bin_prob, mu, logcov = dmm.emission_fxn(zvec)
-    return bin_prob, mu, logcov, zvec
+    params     = dmm.emission_fxn(zvec)
+    if type(params) is not list:
+        params = [params]
+    return params, zvec 
--- model_th/learning.py
+++ model_th/learning.py
@@ -1,24 +1,27 @@
-""" Functions for learning a DMM object """
 import evaluate as DMM_evaluate
 import numpy as np
 from utils.misc import saveHDF5
 import time
-from theano import config
 
+""" Functions for learning a DMM object """
 def learn(dmm, dataset, epoch_start=0, epoch_end=1000, 
           batch_size=200, shuffle=True, savefreq=None, savefile = None, 
           dataset_eval = None):
-    """ Train DMM """
-    assert not dmm.params['validate_only'],'cannot learn in validate only mode'
+    """ Train a DMM using data
+    dmm: DMM object
+    dataset, dataset_eval: <dict> object with data
+    epoch_start, epoch_end, batch_size, savefreq: <int> 
+    savefile: <str> Savefile for intermediate results
+    """
+    assert not dmm.params['validate_only'],'cannot run function in validate only mode'
     N         = dataset['features']['tensor'].shape[0]
     idxlist   = range(N)
     batchlist = np.split(idxlist, range(batch_size,N,batch_size))
-    bound_train_list,bound_valid_list = [],[]
+    bound_train_list, bound_valid_list = [],[]
     epfreq = 1
-    #Set data
     dmm.resetDataset(dataset)
     for epoch in range(epoch_start, epoch_end):
-        #Shuffle
+        #Shuffle and reset batches
         if shuffle:
             np.random.shuffle(idxlist)
             batchlist = np.split(idxlist, range(batch_size,N,batch_size))
@@ -26,18 +29,11 @@ def learn(dmm, dataset, epoch_start=0, epoch_end=1000,
         bound = 0
         for bnum, batch_idx in enumerate(batchlist):
             batch_idx  = batchlist[bnum]
-            #print bnum,'\n'.join(['%.4f'%np.linalg.norm((dmm.tWeights[k]*1.).eval())+' '+k for k in dmm.tWeights])
-            #if np.any(np.isnan(np.array([np.linalg.norm((dmm.tWeights[k]*1.).eval()) for k in dmm.tWeights]))):
-            #    import ipdb;ipdb.set_trace()
-            #if bnum==5:
-            #    import ipdb;ipdb.set_trace()
             batch_bound, p_norm, g_norm, opt_norm, negCLL, KL, anneal = dmm.train_debug(idx=batch_idx)
             if np.any(np.isnan(np.array([batch_bound, p_norm, g_norm, opt_norm, negCLL, KL, anneal]))):
+                print 'Warning: you have encountered a NaN'
                 import ipdb;ipdb.set_trace()
-            #if np.any(np.isnan(np.array([np.linalg.norm((dmm.tWeights[k]*1.).eval()) for k in dmm.tWeights]))):
-            #    import ipdb;ipdb.set_trace()
             bound  += batch_bound
-            ### Display ###
             if epoch%epfreq==0 and bnum%10==0:
                 bval = batch_bound/float(dataset['features']['obs_tensor'][batch_idx].sum())
                 dmm._p(('Bnum: %d, Batch Bound: %.4f, |w|: %.4f, |dw|: %.4f, |w_opt|: %.4f')%(bnum,bval,p_norm, g_norm, opt_norm)) 
@@ -47,7 +43,6 @@ def learn(dmm, dataset, epoch_start=0, epoch_end=1000,
         end_time   = time.time()
         if epoch%epfreq==0:
             dmm._p(('(Ep %d) Bound: %.4f [Took %.4f seconds] ')%(epoch, bound, end_time-start_time))
-        #Save at intermediate stages
         if savefreq is not None and epoch%savefreq==0:
             assert savefile is not None, 'expecting savefile'
             dmm._p(('Saving at epoch %d'%epoch))
@@ -60,7 +55,6 @@ def learn(dmm, dataset, epoch_start=0, epoch_end=1000,
             intermediate['train_bound'] = np.array(bound_train_list)
             saveHDF5(savefile+'-EP'+str(epoch)+'-stats.h5', intermediate)
             dmm.resetDataset(dataset)
-    #Final information to be collected
     retMap = {}
     retMap['train_bound']   = np.array(bound_train_list)
     retMap['valid_bound']   = np.array(bound_valid_list)
