0d616cd9e6e06cfc109abd7422fa50dbc4d48609
Rahul G. Krishnan <rahulk90@gmail.com>
2017-08-08 11:27:30 -0400

    updated py file

--- expt/setup_experiments.py
+++ expt/setup_experiments.py
@@ -8,15 +8,13 @@ from collections import OrderedDict
 import argparse,os
 
 parser = argparse.ArgumentParser(description='Setup Expts')
-parser.add_argument('-hpc','--onHPC',action='store_true') 
 parser.add_argument('-dset','--dataset', default='jsb',action='store')
 parser.add_argument('-ngpu','--num_gpus', default=4,action='store',type=int)
 args = parser.parse_args()
 
 #Main Flags 
-onHPC        = args.onHPC
 DATASET      = args.dataset 
-THFLAGS      = 'THEANO_FLAGS="lib.cnmem=1.,scan.allow_gc=False,compiledir_format=gpu<rand_idx>" ' 
+THFLAGS      = 'THEANO_FLAGS="gpuarray.preallocate=1.,scan.allow_gc=False,compiledir_format=gpu<rand_idx>" ' 
 
 #Get dataset
 dataset      = DATASET.split('-')[0]
@@ -32,24 +30,7 @@ all_expts['nottingham']['DKF-ar'] ='python2.7 train.py -vm R -infm structured -a
 all_expts['musedata']['DKF-ar']   ='python2.7 train.py -vm R -infm structured -ar 5000 -dset <dataset>' 
 all_expts['piano']['DKF-ar']      ='python2.7 train.py -vm R -infm structured -ar 5000 -dset <dataset>' 
 
-if onHPC:
-    DIR = './hpc_'+dataset
-    os.system('rm -rf '+DIR) 
-    os.system('mkdir -p '+DIR)
-    with open('template.q') as ff:
-        template = ff.read()
-    runallcmd    = ''
-    for name in all_expts[dataset]:
-        runcmd  = all_expts[dataset][name].replace('<dataset>',DATASET)+' -uid '+name
-        command = THFLAGS.replace('<rand_idx>',str(np.random.randint(args.num_gpus)))+runcmd
-        with open(DIR+'/'+name+'.q','w') as f:
-            f.write(template.replace('<name>',name).replace('<command>',command))
-        print 'Wrote to:',DIR+'/'+name+'.q'
-        runallcmd+= 'qsub '+name+'.q\n'
-    with open(DIR+'/runall.sh','w') as f:
-        f.write(runallcmd)
-else:
-    for name in all_expts[dataset]:
-        runcmd  = all_expts[dataset][name].replace('<dataset>',DATASET)+' -uid '+name
-        command = THFLAGS.replace('<rand_idx>',str(np.random.randint(args.num_gpus)))+runcmd
-        print command
+for name in all_expts[dataset]:
+    runcmd  = all_expts[dataset][name].replace('<dataset>',DATASET)+' -uid '+name
+    command = THFLAGS.replace('<rand_idx>',str(np.random.randint(args.num_gpus)))+runcmd
+    print command
--- expt/train.py
+++ expt/train.py
@@ -2,21 +2,21 @@ import os,time,sys
 import fcntl,errno
 import socket
 sys.path.append('../')
-from datasets.load import loadDataset
+from dmm_data.load import load
 from parse_args import params 
 from utils.misc import removeIfExists,createIfAbsent,mapPrint,saveHDF5,displayTime,getLowestError
 
-if params['dataset']=='':
-    params['dataset'] = 'jsb'
-dataset = loadDataset(params['dataset'])
+dataset = load(params['dataset'])
 params['savedir']+='-'+params['dataset']
 createIfAbsent(params['savedir'])
 
-#Saving/loading
 for k in ['dim_observations','dim_actions','data_type']:
     params[k] = dataset[k]
 mapPrint('Options: ',params)
 
+"""
+Import files for learning
+"""
 start_time = time.time()
 from   model_th.dmm import DMM
 import model_th.learning as DMM_learn
@@ -24,7 +24,9 @@ import model_th.evaluate as DMM_evaluate
 displayTime('import DMM',start_time, time.time())
 dmm = None
 
-#Remove from params
+"""
+Reload from savefile or train new model
+"""
 start_time = time.time()
 removeIfExists('./NOSUCHFILE')
 reloadFile = params.pop('reloadFile')
@@ -40,9 +42,16 @@ else:
     dmm  = DMM(params, paramFile = pfile)
 displayTime('Building dmm',start_time, time.time())
 
+"""
+Savefile where model checkpoints will be saved
+"""
 savef     = os.path.join(params['savedir'],params['unique_id']) 
 print 'Savefile: ',savef
 start_time= time.time()
+
+"""
+Training loop
+"""
 savedata = DMM_learn.learn(dmm, dataset['train'], dataset['mask_train'], 
                                 epoch_start =0 , 
                                 epoch_end = params['epochs'], 
@@ -55,15 +64,21 @@ savedata = DMM_learn.learn(dmm, dataset['train'], dataset['mask_train'],
                                 shuffle    = False
                                 )
 displayTime('Running DMM',start_time, time.time()         )
+
 dmm = None
-""" Load the best DMM based on the validation error """
+
+""" Reload the best DMM based on the validation error """
 epochMin, valMin, idxMin = getLowestError(savedata['valid_bound'])
 reloadFile= pfile.replace('-config.pkl','')+'-EP'+str(int(epochMin))+'-params.npz'
+
 print 'Loading from : ',reloadFile
 params['validate_only']          = True
 dmm_best                         = DMM(params, paramFile = pfile, reloadFile = reloadFile)
+
+"""
+Evaluate on the test set
+"""
 additional                       = {}
-savedata['bound_test_best']      = DMM_evaluate.evaluateBound(dmm_best,  dataset['test'], dataset['mask_test'], S = 2, batch_size = params['batch_size'], additional =additional) 
-savedata['ll_test_best']         = DMM_evaluate.impSamplingNLL(dmm_best, dataset['test'], dataset['mask_test'], S = 2000, batch_size = params['batch_size'])
+savedata['bound_test']      = DMM_evaluate.evaluateBound(dmm_best,  dataset['test'], dataset['mask_test'], S = 2, batch_size = params['batch_size'], additional =additional) 
 saveHDF5(savef+'-final.h5',savedata)
-print 'Experiment Name: <',params['expt_name'],'> Test Bound: ',savedata['bound_test_best'],' ',savedata['bound_tsbn_test_best'],' ',savedata['ll_test_best']
+print 'Experiment Name: <',params['expt_name'],'> Test Bound: ',savedata['bound_test']
